2af22d7d9010141a58cc3187188f60c06df8e8b4
bmonte <bmonte@outlook.com>
2020-06-07 16:27:15 -0300

    🔧 Adicionando configuração do banco de dados

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3333;
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`🚀: Server running on port ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`DATABASE_URL >> ${process.env.DATABASE_URL}`);
 });
